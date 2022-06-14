026375160c9277c3321710952692a17cc6e9f3bb
Thanawat Yodnil <littleboycoding@gmail.com>
2022-06-14 09:05:54 +0700

    Added rollup bundler to project

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import typescript from "@rollup/plugin-typescript";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import nodePolyfills from "rollup-plugin-polyfill-node";
+
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+export default {
+  input: "src/index.ts",
+  output: {
+    intro: "const global = globalThis;",
+    dir: "dist",
+    format: "umd",
+    name: "candyMachine",
+  },
+  plugins: [
+    typescript(),
+    commonjs({
+      extensions: [".js", ".ts"],
+    }),
+    json(),
+    nodePolyfills(),
+    resolve({
+      preferBuiltins: true,
+      extensions: [".js", ".ts"],
+    }),
+  ],
+};
